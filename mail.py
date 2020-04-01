caa4b247d7d983569648ee103d2df23e9f2a5c84
Eusebio Rubio <35926547+euruba@users.noreply.github.com>
2020-04-01 12:07:54 +0200

    Update mail.py
    Adding server option

--- mail.py
+++ mail.py
@@ -5,20 +5,21 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
  
 if len(sys.argv) < 6:
-    print 'Usage: python ~/mail.py "user" "password" "email@domain.com" "subject" "message"'
+    print 'Usage: python ~/mail.py "user" "password" "smtp-server" "email@domain.com" "subject" "message"'
 else:
-    toaddr = sys.argv[3] # Email 
+    toaddr = sys.argv[4] # Email 
     fromaddr = sys.argv[1]
     password = sys.argv[2]
+    smtpServer = sys.argv[3]
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = sys.argv[4] # Asunto
+    msg['Subject'] = sys.argv[5] # Subject
  
-    body = sys.argv[5] # Mensaje
+    body = sys.argv[6] # Message
     msg.attach(MIMEText(body, 'plain'))
  
-    server = smtplib.SMTP('smtp.1and1.es', 25)
+    server = smtplib.SMTP(smtpServer, 25)
     server.login(fromaddr, password)
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
